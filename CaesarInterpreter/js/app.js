57e77b08dd74fdcbb563e872b4f12ae0ec8ff547
Seffra Stationary <hampus@avekvist.com>
2017-11-01 23:05:22 +0000

    Fixed basically all errors. Fantastic, right? Not sure about all different characters yet, Regex didn't really want to work you know.

--- CaesarInterpreter/js/app.js
+++ CaesarInterpreter/js/app.js
@@ -1,29 +1,93 @@
 var caesarApp = angular.module("caesarApp", []);
 
 caesarApp.controller("MainController", function($scope) {
-    $scope.recent = "none";
+    $scope.testtext = "";
+    $scope.swedish = false;
+    $scope.english = false;
     $scope.encrypt = function(message) {
         var newMessage = "";
         for(var i = 0; i < message.length; i++) {
             for(var j = 0; j < $scope.characters; j++) {
                 var letterToInsert = j + $scope.crypticValue;
-/*
-                if(letterToInsert > $scope.characters) {
-
-                } else if(letterToInsert < 0) {
-                    letterToInsert = $scope.characters - letterToInsert;
+                
+                if(letterToInsert < 0) {
+                    letterToInsert = $scope.characters + letterToInsert;
                 }
-*/              
-                if(message[i] == $scope.lCharacters[j])
-                    newMessage += $scope.lCharacters[letterToInsert];
-                else if(message[i] == $scope.sCharacters[j])
-                    newMessage += $scope.sCharacters[letterToInsert];
-            }
-            
 
-// Regular Expression (RegEx) to make it only be non-alphabetical characters
-            if(message[i] == /[^a-zA-Z]/)
-                newMessage += message[i];
+                if(message[i] == $scope.lCharacters[j]) {
+                    newMessage += $scope.lCharacters[letterToInsert];
+                }
+                else if(message[i] == $scope.sCharacters[j]) {
+                    newMessage += $scope.sCharacters[letterToInsert];
+                }
+
+                if($scope.swedish != false) {
+                    if(message[i] == ' ' || message[i] == '?' ||
+                       message[i] == '\n'|| message[i] == '.' ||
+                       message[i] == ',' || message[i] == '(' ||
+                       message[i] == ')' || message[i] == '{' ||
+                       message[i] == '}' || message[i] == '1' ||
+                       message[i] == '2' || message[i] == '3' ||
+                       message[i] == '4' || message[i] == '5' ||
+                       message[i] == '6' || message[i] == '7' ||
+                       message[i] == '8' || message[i] == '9' ||
+                       message[i] == '0' || message[i] == '+' ||
+                       message[i] == '-' || message[i] == '/' ||
+                       message[i] == '*' || message[i] == '=' ||
+                       message[i] == '!' || message[i] == '"' ||
+                       message[i] == '\''|| message[i] == '\\'||
+                       message[i] == '#' || message[i] == '£' ||
+                       message[i] == '€' || message[i] == 'µ' ||
+                       message[i] == '<' || message[i] == '>' ||
+                       message[i] == '|' || message[i] == '^' ||
+                       message[i] == '¨' || message[i] == '~' ||
+                       message[i] == '¤' || message[i] == '%' ||
+                       message[i] == '&' || message[i] == '$' ||
+                       message[i] == '§' || message[i] == '½' ||
+                       message[i] == '´' || message[i] == '`' ||
+                       message[i] == '_' || message[i] == ':' ||
+                       message[i] == ';' || message[i] == ''  ||
+                       message[i] == '[' || message[i] == ']' ||
+                       message[i] == '@') {
+                        newMessage += message[i];
+                        break;
+                    }
+                } else if($scope.english != false) {
+                    if(message[i] == ' ' || message[i] == '?' ||
+                       message[i] == '\n'|| message[i] == '.' ||
+                       message[i] == ',' || message[i] == '(' ||
+                       message[i] == ')' || message[i] == '{' ||
+                       message[i] == '}' || message[i] == '1' ||
+                       message[i] == '2' || message[i] == '3' ||
+                       message[i] == '4' || message[i] == '5' ||
+                       message[i] == '6' || message[i] == '7' ||
+                       message[i] == '8' || message[i] == '9' ||
+                       message[i] == '0' || message[i] == '+' ||
+                       message[i] == '-' || message[i] == '/' ||
+                       message[i] == '*' || message[i] == '=' ||
+                       message[i] == '!' || message[i] == '"' ||
+                       message[i] == '\''|| message[i] == '\\'||
+                       message[i] == '#' || message[i] == '£' ||
+                       message[i] == '€' || message[i] == 'µ' ||
+                       message[i] == '<' || message[i] == '>' ||
+                       message[i] == '|' || message[i] == '^' ||
+                       message[i] == '¨' || message[i] == '~' ||
+                       message[i] == 'å' || message[i] == 'Å' ||
+                       message[i] == 'ä' || message[i] == 'Ä' ||
+                       message[i] == 'ö' || message[i] == 'Ö' ||
+                       message[i] == '¤' || message[i] == '%' ||
+                       message[i] == '&' || message[i] == '$' ||
+                       message[i] == '§' || message[i] == '½' ||
+                       message[i] == '´' || message[i] == '`' ||
+                       message[i] == '_' || message[i] == ':' ||
+                       message[i] == ';' || message[i] == ''  ||
+                       message[i] == '[' || message[i] == ']' ||
+                       message[i] == '@') {
+                        newMessage += message[i];
+                        break;
+                    }
+                }
+            }
         }
 
         return newMessage;
@@ -34,23 +98,85 @@ caesarApp.controller("MainController", function($scope) {
         for(var i = 0; i < message.length; i++) {
             for(var j = 0; j < $scope.characters; j++) {
                 var letterToInsert = j - $scope.crypticValue;
-
-                if(letterToInsert > $scope.characters) {
-                    
-                } else if(letterToInsert < 0) {
+                
+                if(letterToInsert < 0) {
                     letterToInsert = $scope.characters + letterToInsert;
                 }
                 
-                if(message[i] == $scope.lCharacters[j])
+                if(message[i] == $scope.lCharacters[j]) {
                     newMessage += $scope.lCharacters[letterToInsert];
-                else if(message[i] == $scope.sCharacters[j])
+                } else if(message[i] == $scope.sCharacters[j]) {
                     newMessage += $scope.sCharacters[letterToInsert];
+                }
+
+                if($scope.swedish != false) {
+                    if(message[i] == ' ' || message[i] == '?' ||
+                       message[i] == '\n'|| message[i] == '.' ||
+                       message[i] == ',' || message[i] == '(' ||
+                       message[i] == ')' || message[i] == '{' ||
+                       message[i] == '}' || message[i] == '1' ||
+                       message[i] == '2' || message[i] == '3' ||
+                       message[i] == '4' || message[i] == '5' ||
+                       message[i] == '6' || message[i] == '7' ||
+                       message[i] == '8' || message[i] == '9' ||
+                       message[i] == '0' || message[i] == '+' ||
+                       message[i] == '-' || message[i] == '/' ||
+                       message[i] == '*' || message[i] == '=' ||
+                       message[i] == '!' || message[i] == '"' ||
+                       message[i] == '\''|| message[i] == '\\'||
+                       message[i] == '#' || message[i] == '£' ||
+                       message[i] == '€' || message[i] == 'µ' ||
+                       message[i] == '<' || message[i] == '>' ||
+                       message[i] == '|' || message[i] == '^' ||
+                       message[i] == '¨' || message[i] == '~' ||
+                       message[i] == '¤' || message[i] == '%' ||
+                       message[i] == '&' || message[i] == '$' ||
+                       message[i] == '§' || message[i] == '½' ||
+                       message[i] == '´' || message[i] == '`' ||
+                       message[i] == '_' || message[i] == ':' ||
+                       message[i] == ';' || message[i] == ''  ||
+                       message[i] == '[' || message[i] == ']' ||
+                       message[i] == '@') {
+                        newMessage += message[i];
+                        break;
+                    }
+                } else if($scope.english != false) {
+                    if(message[i] == ' ' || message[i] == '?' ||
+                       message[i] == '\n'|| message[i] == '.' ||
+                       message[i] == ',' || message[i] == '(' ||
+                       message[i] == ')' || message[i] == '{' ||
+                       message[i] == '}' || message[i] == '1' ||
+                       message[i] == '2' || message[i] == '3' ||
+                       message[i] == '4' || message[i] == '5' ||
+                       message[i] == '6' || message[i] == '7' ||
+                       message[i] == '8' || message[i] == '9' ||
+                       message[i] == '0' || message[i] == '+' ||
+                       message[i] == '-' || message[i] == '/' ||
+                       message[i] == '*' || message[i] == '=' ||
+                       message[i] == '!' || message[i] == '"' ||
+                       message[i] == '\''|| message[i] == '\\'||
+                       message[i] == '#' || message[i] == '£' ||
+                       message[i] == '€' || message[i] == 'µ' ||
+                       message[i] == '<' || message[i] == '>' ||
+                       message[i] == '|' || message[i] == '^' ||
+                       message[i] == '¨' || message[i] == '~' ||
+                       message[i] == 'å' || message[i] == 'Å' ||
+                       message[i] == 'ä' || message[i] == 'Ä' ||
+                       message[i] == 'ö' || message[i] == 'Ö' ||
+                       message[i] == '¤' || message[i] == '%' ||
+                       message[i] == '&' || message[i] == '$' ||
+                       message[i] == '§' || message[i] == '½' ||
+                       message[i] == '´' || message[i] == '`' ||
+                       message[i] == '_' || message[i] == ':' ||
+                       message[i] == ';' || message[i] == ''  ||
+                       message[i] == '[' || message[i] == ']' ||
+                       message[i] == '@') {
+                        newMessage += message[i];
+                        break;
+                    }
+                }
             }
             
-
-// Regular Expression (RegEx) to make it only be non-alphabetical characters
-            if(message[i] == /[^a-zA-Z]/)
-                newMessage += message[i];
         }
 
         return newMessage;
@@ -60,16 +186,48 @@ caesarApp.controller("MainController", function($scope) {
     $scope.output = $scope.encrypt("Encrypted Message");
 
     $scope.crypticValue = 4;
-    $scope.characters = 26;
-    $scope.lCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
-                          'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-                          'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-    $scope.sCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
-                          'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-                          's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
     $scope.updateValue = function() {
         parseInt($scope.crypticValue);
+        $scope.output = $scope.encrypt($scope.input);
+    }
+
+    $scope.activateEnglish = function() {
+        $scope.testtext = "ENGLOSH";
+        $scope.characters = 26;
+        $scope.swedish = false;
+        $scope.lCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+                              'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+                              'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A',
+                              'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+                              'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+                              'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y'];
+        $scope.sCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+                              'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                              's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a',
+                              'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+                              'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+                              'r', 's', 't', 'u', 'v', 'w', 'x', 'y'];
+    }
+
+    $scope.activateSwedish = function() {
+        $scope.testtext = "SWEDOSH";
+        $scope.characters = 29;
+        $scope.english = false;
+        $scope.lCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+                              'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+                              'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Å',
+                              'Ä', 'Ö', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
+                              'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                              'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+                              'Z', 'Å', 'Ä'];
+        $scope.sCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+                              'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                              's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'å',
+                              'ä', 'ö', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+                              'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+                              'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+                              'z', 'å', 'ä'];
     }
 
     var slider = document.getElementById("crypt-range");
@@ -78,4 +236,5 @@ caesarApp.controller("MainController", function($scope) {
     }
 
     $scope.output = $scope.encrypt("Encrypted Message");
+    $scope.activateEnglish();
 });
