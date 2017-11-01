7bc29a2bded9ffe051b82b1dd6f88fbf3e82509b
Seffra Stationary <hampus@avekvist.com>
2017-11-01 14:56:23 +0000

    Added the Caesar Interpreteter. Makes Caesar crypt easier to write.

---
+++ CaesarInterpreter/js/app.js
@@ -0,0 +1,45 @@
+var caesarApp = angular.module("caesarApp", []);
+
+caesarApp.controller("MainController", function($scope) {
+    $scope.encrypt = function(message) {
+        var newMessage = "";
+        for(var i = 0; i < message.length; i++) {
+            for(var j = 0; j < $scope.characters; j++) {
+                var letterToInsert = j + $scope.crypticValue;
+                if(letterToInsert > $scope.characters) {
+
+                } else if(letterToInsert < 0) {
+                    letterToInsert = $scope.characters - letterToInsert;
+                }
+                
+                if(message[i] == $scope.lCharacters[j])
+                    newMessage += $scope.lCharacters[letterToInsert];
+                else if(message[i] == $scope.sCharacters[j])
+                    newMessage += $scope.sCharacters[letterToInsert];
+            }
+            
+//            if(message[i] != /[a-zA-Z]/)
+//                newMessage += message[i];
+        }
+
+        return newMessage;
+    }
+
+    $scope.input = "Normal Message";
+    $scope.output = $scope.encrypt("Encrypted Message");
+
+    $scope.crypticValue = 4;
+    $scope.characters = 26;
+    $scope.lCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+                          'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+                          'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    $scope.sCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+                          'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                          's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+    $scope.updateValue = function() {
+        parseInt($scope.crypticValue);
+    }
+    
+    $scope.output = $scope.encrypt("Encrypted Message");
+});
